15d218ddfb9bc1dee5752bf1f68d450a0ebffc85
Crystal Brock <65456479+crystalbrock619@users.noreply.github.com>
2022-02-23 14:28:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//Search My Site
+function search() {
+    var site = "crystalbrock.org"
+    //get user text from search text box
+    var lookFor = document.getElementById("txtlookfor").value;
+    //if search box not empty do the search
+    if(lookFor.length>0) {
+      //Build URL for the search
+      var query="http://www.google.com/search?q=" + encodeURIComponent(lookFor) + " site:" + site;
+      //Set Address bar equal to query
+      location.href=query
+    }
+    else{
+        
+    }
+}
 function contactbutton(){
   window.open("./contact.html"); 
   }
